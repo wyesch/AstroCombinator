8d5bd1aace6620e4d6d0a9f494a8e6749773dbb5
Wyatt Esch <wyesch@ad.unc.edu>
2024-11-02 11:43:28 -0400

    Intial Commit

--- src/main/java/Image.java
+++ src/main/java/Image.java
@@ -1,7 +1,8 @@
 import java.awt.*;
 
 public interface Image {
-    //*
+    //* returns pixel color at x and y
     Color getPixelColor(int x,int y);
+    //* returns array getsize[0] = width getsize[1] = height
     int[] getSize();
 }
